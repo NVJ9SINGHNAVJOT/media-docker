313f7fdfffe59628a8e65b56f709d578782e5a04
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-08-26 14:34:35 +0530

    build: taskfile
    updated

--- middleware/default.go
+++ middleware/default.go
@@ -3,8 +3,8 @@ package middleware
 import (
 	"time"
 
-	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/go-chi/httprate"
 )
