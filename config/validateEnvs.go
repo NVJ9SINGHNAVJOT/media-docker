ca76f33abcebe27ca39f557d7245ed46a74338e3
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-08-28 19:39:56 +0530

    refactor: env
    env validation and exapmle updated]

--- config/validateEnvs.go
+++ config/validateEnvs.go
@@ -6,15 +6,49 @@ import (
 	"strings"
 )
 
-type MDSEnvironmentConfig struct {
+/*
+	MDC = media-docker-client
+	MDS = media-docker-server
+*/
+
+type MDCEnvironmentConfig struct {
 	Environment    string
 	AllowedOrigins []string
-	ServerKey      string
 	Port           string
-	BASE_URL       string
 }
 
+type MDSEnvironmentConfig struct {
+	MDCEnvironmentConfig
+	ServerKey string
+	BASE_URL  string
+}
+
+var MDCenvs = MDCEnvironmentConfig{}
 var MDSenvs = MDSEnvironmentConfig{}
+
+func ValidateMDCenvs() error {
+
+	environment, exist := os.LookupEnv("ENVIRONMENT")
+	if !exist {
+		return fmt.Errorf("environment is not provided")
+	}
+
+	allowedOrigins, exist := os.LookupEnv("ALLOWED_ORIGINS_CLIENT")
+	if !exist {
+		return fmt.Errorf("allowed origins is not provided")
+	}
+
+	port, exist := os.LookupEnv("CLIENT_PORT")
+	if !exist {
+		return fmt.Errorf("port number is not provided")
+	}
+
+	MDCenvs.Environment = environment
+	MDCenvs.AllowedOrigins = strings.Split(allowedOrigins, ",")
+	MDCenvs.Port = port
+
+	return nil
+}
 
 func ValidateMDSenvs() error {
 
@@ -23,7 +57,7 @@ func ValidateMDSenvs() error {
 		return fmt.Errorf("environment is not provided")
 	}
 
-	allowedOrigins, exist := os.LookupEnv("ALLOWED_ORIGINS")
+	allowedOrigins, exist := os.LookupEnv("ALLOWED_ORIGINS_SERVER")
 	if !exist {
 		return fmt.Errorf("allowed origins is not provided")
 	}
@@ -33,7 +67,7 @@ func ValidateMDSenvs() error {
 		return fmt.Errorf("server key is not provided")
 	}
 
-	port, exist := os.LookupEnv("PORT")
+	port, exist := os.LookupEnv("SERVER_PORT")
 	if !exist {
 		return fmt.Errorf("port number is not provided")
 	}
@@ -48,38 +82,6 @@ func ValidateMDSenvs() error {
 	MDSenvs.ServerKey = serverKey
 	MDSenvs.Port = port
 	MDSenvs.BASE_URL = baseUrl
-
-	return nil
-}
-
-type MDCEnvironmentConfig struct {
-	Environment    string
-	AllowedOrigins []string
-	Port           string
-}
-
-var MDCenvs = MDCEnvironmentConfig{}
-
-func ValidateMDCenvs() error {
-
-	environment, exist := os.LookupEnv("ENVIRONMENT")
-	if !exist {
-		return fmt.Errorf("environment is not provided")
-	}
-
-	allowedOrigins, exist := os.LookupEnv("ALLOWED_ORIGINS")
-	if !exist {
-		return fmt.Errorf("allowed origins is not provided")
-	}
-
-	port, exist := os.LookupEnv("PORT")
-	if !exist {
-		return fmt.Errorf("port number is not provided")
-	}
-
-	MDCenvs.Environment = environment
-	MDCenvs.AllowedOrigins = strings.Split(allowedOrigins, ",")
-	MDCenvs.Port = port
 
 	return nil
 }
