07da5e34059aa6a0062aa54ba6ad341f82610596
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-09-09 14:41:35 +0530

    feat(video api): quality parameter added for video conversion command pointer

--- api/video.go
+++ api/video.go
@@ -3,6 +3,8 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"os/exec"
+	"strconv"
 	"sync"
 
 	"github.com/google/uuid"
@@ -12,6 +14,10 @@ import (
 	"github.com/nvj9singhnavjot/media-docker/worker"
 )
 
+type videoRequest struct {
+	Quality string `json:"quality" validate:"omitempty,customVideoQuality"`
+}
+
 func Video(w http.ResponseWriter, r *http.Request) {
 
 	_, header, err := r.FormFile("videoFile")
@@ -19,8 +25,15 @@ func Video(w http.ResponseWriter, r *http.Request) {
 		helper.Response(w, http.StatusBadRequest, "error reading file", err.Error())
 		return
 	}
-
 	videoPath := header.Header.Get("path")
+
+	var req videoRequest
+	// Parse the JSON request and populate the VideoRequest struct.
+	if err := helper.ValidateRequest(r, &req); err != nil {
+		helper.Response(w, http.StatusBadRequest, "invalid data", err.Error())
+		go pkg.DeleteFile(videoPath)
+		return
+	}
 
 	id := uuid.New().String()
 	outputPath := fmt.Sprintf("%s/videos/%s", helper.Constants.MediaStorage, id)
@@ -32,11 +45,25 @@ func Video(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var cmd *exec.Cmd
+
+	if req.Quality != "" {
+		quality, err := strconv.Atoi(req.Quality)
+		if err != nil {
+			helper.Response(w, http.StatusInternalServerError, "error while getting quality for video", err.Error())
+			go pkg.DeleteFile(videoPath)
+			return
+		}
+		cmd = pkg.ConvertVideo(videoPath, outputPath, quality)
+	} else {
+		cmd = pkg.ConvertVideo(videoPath, outputPath)
+	}
+
 	var executeError = false
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	worker.AddInVideoChannel(pkg.ConvertVideo(videoPath, outputPath), &wg, &executeError)
+	worker.AddInVideoChannel(cmd, &wg, &executeError)
 
 	wg.Wait()
 
