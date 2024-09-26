8e3a4e51bef124f86cbc449689e2de3662ee9b61
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-09-26 23:31:24 +0530

    fix(request map): filePath replaced with id

--- api/image.go
+++ api/image.go
@@ -43,7 +43,7 @@ func Image(w http.ResponseWriter, r *http.Request) {
 	// Pass the struct to the Kafka producer
 	if err := serverKafka.KafkaProducer.Produce("image", message); err != nil {
 		pkg.AddToFileDeleteChan(imagePath)
-		serverKafka.ImageRequestMap.Delete(imagePath)
+		serverKafka.ImageRequestMap.Delete(id)
 		helper.Response(w, http.StatusInternalServerError, "error sending Kafka message", err.Error())
 		return
 	}
@@ -52,7 +52,7 @@ func Image(w http.ResponseWriter, r *http.Request) {
 	responseSuccess := <-responseChannel
 
 	// Delete the channel from the map once processing is complete
-	serverKafka.ImageRequestMap.Delete(imagePath)
+	serverKafka.ImageRequestMap.Delete(id)
 
 	// Check if the processing was successful or failed
 	if !responseSuccess {
