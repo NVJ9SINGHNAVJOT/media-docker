85f15433d6b3e272bdf27a7c9aa196b7063676ab
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-10-11 23:24:21 +0530

    refactor(system-design): :zap: kafka topics changed for response

--- kafka_config.sh
+++ kafka_config.sh
@@ -3,14 +3,11 @@
 # Define topics and their default partition values
 declare -A topics_and_partitions=(
     ["video"]=100
-    ["video-response"]=50
     ["video-resolutions"]=100
-    ["video-resolutions-response"]=50
     ["image"]=100
-    ["image-response"]=50
     ["audio"]=50
-    ["audio-response"]=25
     ["delete-file"]=20
+    ["media-docker-response"]=100
 )
 
 # Export the topics_and_partitions array for use in other scripts
