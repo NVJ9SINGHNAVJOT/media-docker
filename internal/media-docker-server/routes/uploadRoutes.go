86af30902a3af392c49d619eebbc10444dfb19a1
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-10-23 13:16:10 +0530

    feat(chunks): :sparkles: upload route updated for chunks api

--- internal/media-docker-server/routes/uploadRoutes.go
+++ internal/media-docker-server/routes/uploadRoutes.go
@@ -1,18 +1,17 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/nvj9singhnavjot/media-docker/api"
-	"github.com/nvj9singhnavjot/media-docker/middleware"
 )
 
 func UploadRoutes() func(router chi.Router) {
 	return func(router chi.Router) {
-		router.Post("/video", middleware.FileStorage("videoFile", http.HandlerFunc(api.Video)))
-		router.Post("/videoResolutions", middleware.FileStorage("videoFile", http.HandlerFunc(api.VideoResolutions)))
-		router.Post("/image", middleware.FileStorage("imageFile", http.HandlerFunc(api.Image)))
-		router.Post("/audio", middleware.FileStorage("audioFile", http.HandlerFunc(api.Audio)))
+		router.Post("/chunksStorage", api.ChunksStorage)
+		router.Post("/fileStorage", api.FileStorage)
+		router.Post("/video", api.Video)
+		router.Post("/videoResolutions", api.VideoResolutions)
+		router.Post("/image", api.Image)
+		router.Post("/audio", api.Audio)
 	}
 }
